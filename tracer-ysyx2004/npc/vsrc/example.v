ea5b26938368b9fc325938cc706ed5013e183b36
WCX1024979076 <1024979076@qq.com>
2022-03-29 11:38:59 +0800

    delete  tracer-ysyx2204 dict

--- tracer-ysyx2004/npc/vsrc/example.v
+++
@@ -1,8 +0,0 @@
-module top(
-    input a,
-    input b,
-    output f
-);
-
-assign f=a^b;
-endmodule
--- tracer-ysyx2004/npc/vsrc/m_mux21.v
+++
@@ -1,7 +0,0 @@
-module m_mux21(a,b,s,y);
-    input a,b,s;
-    output y;
-
-    assign y=(~s&a)|(s&b);
-
-    endmodule
