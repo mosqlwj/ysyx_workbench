09db13d9cb8e35cae8bf62eb0b273aebb79b392a
tracer-ysyx2204 <tracer@ysyx.org>
2022-05-02 22:28:56 +0800

    >  sim RTL ysyx_22040785 王春祥 Linux VM-24-9-ubuntu 5.4.0-99-generic #112-Ubuntu SMP Thu Feb 3 13:50:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:28:56 up 20 days, 23:05,  3 users,  load average: 0.07, 0.08, 0.07

--- tracer-ysyx2204/npc/csrc/main.cpp
+++ tracer-ysyx2204/npc/csrc/main.cpp
@@ -45,6 +45,8 @@ void exec_once()
   char p[100] = {0};
   puts("123");
   disassemble(p, 100, cpu_npc.pc, top->io_Inst, 4);
+  puts("123");
+
   printf("pc=%lx,inst=%lx,disassm=%s", cpu_npc.pc, top->io_Inst, p);
 #ifdef CONFIG_VCD
   m_trace->dump(sim_time++);
