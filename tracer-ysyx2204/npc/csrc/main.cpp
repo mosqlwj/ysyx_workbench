ffa731d31a8a20b8ea2c9f70cfa2442517fd6218
tracer-ysyx2204 <tracer@ysyx.org>
2022-05-02 20:04:08 +0800

    >  sim RTL ysyx_22040785 王春祥 Linux VM-24-9-ubuntu 5.4.0-99-generic #112-Ubuntu SMP Thu Feb 3 13:50:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:04:08 up 20 days, 20:40,  3 users,  load average: 0.00, 0.06, 0.03

---
+++ tracer-ysyx2204/npc/csrc/main.cpp
@@ -0,0 +1,80 @@
+#include "npc.h"
+
+CPU_state cpu_npc;
+uint64_t *cpu_gpr = NULL;
+vluint64_t sim_time = 0;
+VMain *top = nullptr;
+VerilatedContext *contextp = nullptr;
+#ifdef CONFIG_VCD
+VerilatedVcdC *m_trace = nullptr;
+#endif
+
+int check_regs_npc(CPU_state ref_cpu);
+void init_so(char *ref_so_file, long img_size);
+
+void cpu_sim_once()
+{
+  top->clock = 0, top->eval();
+  top->clock = 1, top->eval();
+}
+
+void init_npc()
+{
+  top->reset = 1;
+  for (int i = 1; i <= 10; i++)
+    cpu_sim_once();
+  top->reset = 0;
+}
+
+void exec_once()
+{
+  cpu_sim_once();
+#ifdef CONFIG_VCD
+  m_trace->dump(sim_time++);
+#endif
+#ifdef CONFIG_DIFFTEST
+  assert(ref_difftest_exec);
+  ref_difftest_exec(1);
+  CPU_state ref_cpu;
+  ref_difftest_regcpy(&ref_cpu, DIFFTEST_TO_DUT);
+  printf("check at nemu_pc=%lx, npc_pc=%lx\n", cpu_npc.pc, ref_cpu.pc);
+  if (!check_regs_npc(ref_cpu))
+    exit_npc(-1);
+#endif
+}
+
+int main(int argc, char **argv, char **env)
+{
+  long size = 0;
+  if (argc == 2)
+  {
+    if (strlen(argv[1]) != 0)
+    {
+      printf("ld:%s\n", argv[1]);
+      size = ld(argv[1]);
+    }
+  }
+  contextp = new VerilatedContext;
+  contextp->commandArgs(argc, argv);
+  top = new VMain{contextp};
+
+#ifdef CONFIG_VCD
+  Verilated::traceEverOn(true);
+  m_trace = new VerilatedVcdC;
+  top->trace(m_trace, 5);
+  m_trace->open("waveform.vcd");
+#endif
+
+  init_npc();
+
+#ifdef CONFIG_DIFFTEST
+  init_so("/home/wcx/Desktop/ysyx-workbench/nemu/build/riscv64-nemu-interpreter-so", size);
+#endif
+  while (1)
+  {
+    exec_once();
+  }
+
+  exit_npc(0);
+  return 0;
+}
