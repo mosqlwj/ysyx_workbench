e41cc3c4f98b3c85c0f28dc606cba03cc9a8d205
tracer-ysyx2204 <tracer@ysyx.org>
2022-05-02 22:30:05 +0800

    >  sim RTL ysyx_22040785 王春祥 Linux VM-24-9-ubuntu 5.4.0-99-generic #112-Ubuntu SMP Thu Feb 3 13:50:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:30:05 up 20 days, 23:06,  3 users,  load average: 0.16, 0.10, 0.08

--- tracer-ysyx2204/npc/csrc/main.cpp
+++ tracer-ysyx2204/npc/csrc/main.cpp
@@ -44,9 +44,9 @@ void exec_once()
   cpu_sim_once();
   char p[100] = {0};
   puts("123");
-  printf("pc=%lx,inst=%lx,disassm=%s\n", cpu_npc.pc, top->io_Inst, p);
+  printf("pc=%lx,inst=%lx,disassem=%s\n", cpu_npc.pc, top->io_Inst, p);
   disassemble(p, 100, cpu_npc.pc, top->io_Inst, 4);
-  printf("pc=%lx,inst=%lx,disassm=%s", cpu_npc.pc, top->io_Inst, p);
+  printf("pc=%lx,inst=%lx,disassem=%s\n", cpu_npc.pc, top->io_Inst, p);
 #ifdef CONFIG_VCD
   m_trace->dump(sim_time++);
 #endif
