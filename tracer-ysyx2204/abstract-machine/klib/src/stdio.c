d7b227d3d63b17e8f426c476c875c373fe6d30b1
tracer-ysyx2204 <tracer@ysyx.org>
2022-05-03 16:29:19 +0800

    >  compile NEMU ysyx_22040785 王春祥 Linux VM-24-9-ubuntu 5.4.0-99-generic #112-Ubuntu SMP Thu Feb 3 13:50:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:29:19 up 21 days, 17:06,  7 users,  load average: 0.43, 0.94, 0.98

--- tracer-ysyx2204/abstract-machine/klib/src/stdio.c
+++ tracer-ysyx2204/abstract-machine/klib/src/stdio.c
@@ -8,6 +8,43 @@
 int printf(const char *fmt, ...)
 {
   // panic("Not implemented");
+  char out[2048];
+  va_list va_ptr;
+  va_start(va_ptr, fmt);
+  int cnt = 0;
+  for (int i = 0; fmt[i]; i++)
+  {
+    if (fmt[i] != '%')
+    {
+      out[cnt++] = fmt[i];
+      continue;
+    }
+    int num = 0, num_b_cnt = 0;
+    int num_b[20] = {0};
+    char *str = NULL;
+    switch (fmt[i + 1])
+    {
+    case 'd':
+      num_b_cnt = 0;
+      num = va_arg(va_ptr, int);
+      while (num != 0)
+      {
+        num_b[++num_b_cnt] = num % 10;
+        num /= 10;
+      }
+      for (int i = num_b_cnt; i >= 1; i--)
+        out[cnt++] = (char)(num_b[i] + '0');
+      break;
+    case 's':
+      str = va_arg(va_ptr, char *);
+      for (int i = 0; str[i]; i++)
+        out[cnt++] = str[i];
+      break;
+    }
+    i++;
+  }
+  out[cnt++] = '\0';
+  putstr(out);
   return 0;
 }
 
