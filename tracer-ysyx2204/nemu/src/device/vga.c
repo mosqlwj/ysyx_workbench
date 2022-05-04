e6dd9fc7e9cf53e0c4b87a931e4d606408508014
tracer-ysyx2204 <tracer@ysyx.org>
2022-05-04 21:38:19 +0800

    >  compile NEMU ysyx_22040785 王春祥 Linux VM-24-9-ubuntu 5.4.0-99-generic #112-Ubuntu SMP Thu Feb 3 13:50:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:38:19 up 22 days, 22:15,  4 users,  load average: 0.25, 0.29, 0.33

--- tracer-ysyx2204/nemu/src/device/vga.c
+++ tracer-ysyx2204/nemu/src/device/vga.c
@@ -62,6 +62,7 @@ void vga_update_screen() {
 
 void init_vga() {
   vgactl_port_base = (uint32_t *)new_space(8);
+  Log("%d %d\n",screen_width() , screen_height());
   vgactl_port_base[0] = (screen_width() << 16) | screen_height(); 
 #ifdef CONFIG_HAS_PORT_IO
   add_pio_map ("vgactl", CONFIG_VGA_CTL_PORT, vgactl_port_base, 8, NULL);
