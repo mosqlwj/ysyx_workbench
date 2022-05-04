c40964580bb3a6c8ce821ac274f5bd1f8db75f9a
tracer-ysyx2204 <tracer@ysyx.org>
2022-05-04 22:26:08 +0800

    >  compile NEMU ysyx_22040785 王春祥 Linux VM-24-9-ubuntu 5.4.0-99-generic #112-Ubuntu SMP Thu Feb 3 13:50:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:26:08 up 22 days, 23:02,  4 users,  load average: 0.39, 0.37, 0.39

--- tracer-ysyx2204/abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ tracer-ysyx2204/abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -17,7 +17,7 @@ void __am_gpu_init() {
 void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
   *cfg = (AM_GPU_CONFIG_T) {
     .present = true, .has_accel = false,
-    .width = 0, .height = 0,
+    .width = width, .height = height,
     .vmemsz = 0
   };
 }
