0b21269b5a71207e0fad718ea5b5b907ea88d648
tracer-ysyx2204 <tracer@ysyx.org>
2022-05-04 21:37:12 +0800

    >  compile NEMU ysyx_22040785 王春祥 Linux VM-24-9-ubuntu 5.4.0-99-generic #112-Ubuntu SMP Thu Feb 3 13:50:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:37:12 up 22 days, 22:13,  4 users,  load average: 0.35, 0.30, 0.33

---
+++ tracer-ysyx2204/abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -0,0 +1,33 @@
+#include <am.h>
+#include <nemu.h>
+#include <stdio.h>
+#define SYNC_ADDR (VGACTL_ADDR + 4)
+
+void __am_gpu_init() {
+  int i;
+  uint32_t vga_ctl = inl(VGACTL_ADDR);
+  int w = (vga_ctl>>16);  // TODO: get the correct width
+  int h = (vga_ctl)&((1<<16)-1);  // TODO: get the correct height
+  printf("%d %d\n",w,h);
+  uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
+  for (i = 0; i < w * h; i ++) fb[i] = i;
+  outl(SYNC_ADDR, 1);
+}
+
+void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
+  *cfg = (AM_GPU_CONFIG_T) {
+    .present = true, .has_accel = false,
+    .width = 0, .height = 0,
+    .vmemsz = 0
+  };
+}
+
+void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
+  if (ctl->sync) {
+    outl(SYNC_ADDR, 1);
+  }
+}
+
+void __am_gpu_status(AM_GPU_STATUS_T *status) {
+  status->ready = true;
+}
