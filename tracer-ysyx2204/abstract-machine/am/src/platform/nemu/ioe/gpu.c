11b90e983d48af9f28a5a6a8f1d0edaa4289b7c8
tracer-ysyx2204 <tracer@ysyx.org>
2022-05-04 22:34:13 +0800

    >  compile NEMU ysyx_22040785 王春祥 Linux VM-24-9-ubuntu 5.4.0-99-generic #112-Ubuntu SMP Thu Feb 3 13:50:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:34:13 up 22 days, 23:10,  4 users,  load average: 0.40, 0.60, 0.51

--- tracer-ysyx2204/abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ tracer-ysyx2204/abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -31,13 +31,10 @@ void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
     for(int j = 0; j < ctl->h ; j ++){
       int p = ctl->x + i;
       int q = ctl->y + j;
-      printf("pq %d %d\n",p,q);
-      printf("wh %d %d\n",width,height);
-      printf("pq %d\n",p * width + q);
-      printf("fb %d\n",fb[p * width + q]);
-      fb[p * width + q] = ((uint32_t*)ctl->pixels)[++cnt];
+      fb[p * height + q] = ((uint32_t*)ctl->pixels)[++cnt];
     }
   }
+  printf("%d\n",ctl->sync);
   if (ctl->sync) {
     outl(SYNC_ADDR, 1);
   }
