a9ee4ea21214598be14551c3c46488134c4a3f0e
tracer-ysyx2204 <tracer@ysyx.org>
2022-05-04 20:32:51 +0800

    >  compile NEMU ysyx_22040785 王春祥 Linux VM-24-9-ubuntu 5.4.0-99-generic #112-Ubuntu SMP Thu Feb 3 13:50:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:32:51 up 22 days, 21:09,  4 users,  load average: 0.48, 0.37, 0.37

--- tracer-ysyx2204/abstract-machine/am/src/platform/nemu/ioe/input.c
+++ tracer-ysyx2204/abstract-machine/am/src/platform/nemu/ioe/input.c
@@ -3,9 +3,10 @@
 #include <stdio.h>
 #define KEYDOWN_MASK 0x8000
 
+bool is_down[256] = {0};
 void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd)
 {
-  int keycode = inl(KBD_ADDR)%256;
+  int keycode = inl(KBD_ADDR) % 256;
   if (keycode == AM_KEY_NONE)
   {
     kbd->keydown = false;
@@ -13,7 +14,11 @@ void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd)
   }
   else
   {
-    kbd->keydown = true;
+    if (is_down[keycode])
+      kbd->keydown = false;
+    else
+      kbd->keydown = true;
+    is_down[keycode] = !is_down[keycode];
     kbd->keycode = keycode;
   }
 }
